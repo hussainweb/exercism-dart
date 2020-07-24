0bf7c959a545ee286ed538603723f1460ee3bc0e
hw <hussainweb@gmail.com>
2020-07-24 13:26:59 -0400

    Add solution for resistor-color

--- resistor-color/lib/resistor_color.dart
+++ resistor-color/lib/resistor_color.dart
@@ -1,3 +1,19 @@
 class ResistorColor {
   // Put your code here
+  var colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int colorCode(String colorName) {
+    return colors.indexOf(colorName);
+  }
 }
