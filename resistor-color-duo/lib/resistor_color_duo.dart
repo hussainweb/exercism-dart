5dce5a2fb72e8b75e125e4bdca37dda8bb2f0f78
hw <hussainweb@gmail.com>
2020-08-02 09:43:17 -0400

    Add solution for resistor-color-duo

--- resistor-color-duo/lib/resistor_color_duo.dart
+++ resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,20 @@
 class ResistorColorDuo {
-  // Put your code here
+  var colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+
+  int value(List<String> colorNames) {
+    var firstColor = colors.indexOf(colorNames[0]);
+    var secondColor = colors.indexOf(colorNames[1]);
+    return (firstColor * 10) + secondColor;
+  }
 }
