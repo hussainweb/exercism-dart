fb3826276a31ce61e6351bacf20ff4280be14cbe
hw <hussainweb@gmail.com>
2020-08-23 19:45:18 -0400

    Add solution for collatz-conjecture

--- collatz-conjecture/lib/collatz_conjecture.dart
+++ collatz-conjecture/lib/collatz_conjecture.dart
@@ -1,3 +1,22 @@
 class CollatzConjecture {
-  // Put your code here
+  int steps(int n) {
+    if (n <= 0) {
+      throw ArgumentError('Only positive numbers are allowed');
+    }
+
+    var steps = 0;
+    while (n != 1) {
+      switch (n % 2) {
+        case 0:
+          n ~/= 2;
+          break;
+        case 1:
+          n = (3 * n) + 1;
+          break;
+      }
+      steps++;
+    }
+
+    return steps;
+  }
 }
